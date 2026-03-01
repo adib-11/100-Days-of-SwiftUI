08ced0d2019ee5d8c932dc4a4a89c700c1a2a652
Adib Hasan <adib@Adibs-MacBook-Air.local>
2026-03-01 17:51:45 +0600

    feat: complete Day 26 BetterRest initial UI and CoreML integration

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Adib Hasan on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
